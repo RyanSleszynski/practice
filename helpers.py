f22ce1c3e801082dcf1dd48d58d936459bfdecb3
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-03 14:25:55 -0500

    Rewriting a find_min_max function

--- helpers.py
+++ helpers.py
@@ -16,23 +16,38 @@ def is_prime(whole_number):
         else:
             return False
 
+
 def find_min_max(list_of_items, min_or_max):
-    index = 0
+    # This function will return a minimum number or maximum number depending on the second argument
+    # Errors will be raised if list_of_items is null or if min_or_max is NOT "min" or "max"
+    # A for loop and a while loop are used to show possibility of either returning correct results
+
+    index = 1
 
     if len(list_of_items) == 0:
         raise ValueError("Null set has no min or max")
 
+    elif len(list_of_items) == 1:
+        return list_of_items[0]
+
     elif min_or_max == "min":
         minimum = list_of_items[0]
-        while index < len(list_of_items):
-            if minimum > list_of_items[index]:
-                minimum = list_of_items[index]
-            index += 1
+        # For loop replaces the stored value of minimum if index_value is less than minimum
+        for index_value in list_of_items:
+            if minimum > index_value:
+                minimum = index_value
         return minimum
+
+        # While loop below replaces the stored value of minimum if list_of_items[index] is less than minimum
+        # while index < len(list_of_items):
+        #     if minimum > list_of_items[index]:
+        #         minimum = list_of_items[index]
+        #     index += 1
+        # return minimum
 
     elif min_or_max == "max":
         maximum = list_of_items[0]
-        while index > len(list_of_items):
+        while index < len(list_of_items):
             if maximum < list_of_items[index]:
                 maximum = list_of_items[index]
             index += 1
@@ -48,11 +63,12 @@ else:
     print("Failed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
 
 if find_min_max([9, 33, 14, 5, 0], "max") == 33:
-    print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
+    print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "max"))
 else:
     print("Failed find_min_max", find_min_max([9, 33, 14, 5, 0], "max"))
 
-def sort(given_array, order):
 
-    if len(given_array) == 0:
-        return
+# def sort(given_array, order):
+#
+#     if len(given_array) == 0:
+#         return
