3f8d99cc041b12de4605c5e66511c66f42c4eb47
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-03 13:43:10 -0500

    Rewriting a find_min_max function

--- helpers.py
+++ helpers.py
@@ -16,6 +16,41 @@ def is_prime(whole_number):
         else:
             return False
 
+def find_min_max(list_of_items, min_or_max):
+    index = 0
+
+    if len(list_of_items) == 0:
+        raise ValueError("Null set has no min or max")
+
+    elif min_or_max == "min":
+        minimum = list_of_items[0]
+        while index < len(list_of_items):
+            if minimum > list_of_items[index]:
+                minimum = list_of_items[index]
+            index += 1
+        return minimum
+
+    elif min_or_max == "max":
+        maximum = list_of_items[0]
+        while index > len(list_of_items):
+            if maximum < list_of_items[index]:
+                maximum = list_of_items[index]
+            index += 1
+        return maximum
+
+    else:
+        raise ValueError("Second argument must be \"min\" or \"max\"")
+
+
+if find_min_max([9, 33, 14, 5, 0], "min") == 0:
+    print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
+else:
+    print("Failed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
+
+if find_min_max([9, 33, 14, 5, 0], "max") == 33:
+    print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
+else:
+    print("Failed find_min_max", find_min_max([9, 33, 14, 5, 0], "max"))
 
 def sort(given_array, order):
 
