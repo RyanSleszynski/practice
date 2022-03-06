f585ae012ab1e49ca93bc8b0a38a60545efc2b87
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-06 18:56:17 -0500

    Added raises to the function and tested try statements for error handling

--- helpers.py
+++ helpers.py
@@ -5,11 +5,17 @@ def is_prime(whole_number):
     # Returns True if whole_number is prime and False if not prime
     divisor = 3
 
-    if whole_number < 2 or whole_number % 2 == 0:
+    if not isinstance(whole_number, int):
+        raise TypeError("TypeError: Input must be a positive integer or zero")
+    elif whole_number < 0:
+        raise ValueError("ValueError: Negative numbers cannot be prime.")
+    elif whole_number < 2 or whole_number % 2 == 0:
         return False
     else:
         while whole_number % divisor != 0 and divisor < sqrt(whole_number):
             divisor += 2
+            if divisor % 5 == 0:
+                divisor += 2
             print(divisor)
         if divisor >= sqrt(whole_number):
             return True
@@ -28,5 +34,17 @@ try:
 except Exception as e:
     print(e)
     # print("Variable x is a string and cannot be converted to an integer.")
+
+#is_prime("k")
+prime_test = 15877
+# try:
+#     is_prime(prime_test)
+# except ValueError as v:
+#     print(v)
+# except TypeError as t:
+#     print(t)
+
+print(is_prime(prime_test))
+
 
 print("End")
