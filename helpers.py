1bea7e4ebb5aaf999e4d5349e8e30f4226856eec
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-07 09:39:25 -0500

    Updated is_prime and added test cases

--- helpers.py
+++ helpers.py
@@ -5,8 +5,11 @@ def is_prime(whole_number):
     # Returns True if whole_number is prime and False if not prime
     divisor = 3
 
-    if whole_number < 2 or whole_number % 2 == 0:
+    # Checks for special cases and for simple input i.e. divisible by 2 or 5
+    if whole_number < 2 or whole_number % 2 == 0 and whole_number != 2 or whole_number % 5 == 0:
         return False
+    elif whole_number == 2:
+        return True
     else:
         while whole_number % divisor != 0 and divisor < sqrt(whole_number):
             divisor += 2
@@ -15,6 +18,17 @@ def is_prime(whole_number):
             return True
         else:
             return False
+
+# Test Cases for is_prime
+if is_prime(2):
+    print("Passed is_prime(2)")
+else:
+    print("Failed is_prime(2)")
+
+if not is_prime(25):
+    print("Passed is_prime(25)")
+else:
+    print("Failed is_prime(25)")
 
 
 def find_min_max(list_of_items, min_or_max):
@@ -25,7 +39,7 @@ def find_min_max(list_of_items, min_or_max):
     index = 1
 
     if len(list_of_items) == 0:
-        raise ValueError("Null set has no min or max")
+        print("Null set has no min or max")
 
     elif len(list_of_items) == 1:
         return list_of_items[0]
@@ -56,16 +70,6 @@ def find_min_max(list_of_items, min_or_max):
     else:
         raise ValueError("Second argument must be \"min\" or \"max\"")
 
-
-def sort(list_of_items, order):
-    # Function will return a given list in an ordered list either acending or decending
-    # The flag variable remains False until the list has finished sorting
-
-    flag = False
-    while flag == False:
-        for value in list_of_items:
-             if value:
-
 if find_min_max([9, 33, 14, 5, 0], "min") == 0:
     print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "min"))
 else:
@@ -75,6 +79,17 @@ if find_min_max([9, 33, 14, 5, 0], "max") == 33:
     print("Passed find_min_max", find_min_max([9, 33, 14, 5, 0], "max"))
 else:
     print("Failed find_min_max", find_min_max([9, 33, 14, 5, 0], "max"))
+
+def sort(list_of_items, order):
+    # Function will return a given list in an ordered list either acending or decending
+    # The flag variable remains False until the list has finished sorting
+
+    flag = False
+    # while flag == False:
+    #     for value in list_of_items:
+    #          if value:
+
+
 
 
 def sum(list_of_items):
