45e33ab2c7f27df6b9de63a7a9f8a8da93fe1ee7
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-04-23 23:32:11 -0400

    - playing around with monty_carlo testing

--- testing.py
+++ testing.py
@@ -102,171 +102,48 @@ import helpers_recursive
 #         print('Average time taken by', str(function_name) + ":", total_time_taken / number_of_trials)
 
 
-# # ---------------------is_sorted trials with sorted input below--------------------------------
-#
-# max_value = 100
-# number_of_trials = 1000  # you can change this number to make the testing faster.
-# number_of_trials_per_input = 1000
-# # Setting the seed to make sure that the same random list is generated every time with the same parameters
-# random.seed(123)  # If you comment this line, you will get different list every time for the same parameters
-# print("Instancing input...")
-# input_list = [helpers.sort_using_min_max([int(random.random() * max_value) for i in range(number_of_trials)], "asc") for i in range(number_of_trials)]
-# input_list_order = [random.choice(["asc", "desc"]) for i in range(number_of_trials)]
-#
-# print("Input instanced.")
-# print("is_sorted trials with sorted lists commence:")
-# # Time taken in is_sorted
-# total_time_taken = 0
-# for item_list, item_order in zip(input_list, input_list_order):  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers.is_sorted(item_list, item_order)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print('Average time taken by is_sorted using for:', (total_time_taken) / number_of_trials)
-#
-# # Time taken in is_sorted_while
-# # we will use the same setup and input list as recursion so the comparison is fair
-# total_time_taken = 0
-# for item_list, item_order in zip(input_list, input_list_order):  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers.is_sorted_while(item_list, item_order)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print("Average time taken by is_sorted using while:", (total_time_taken) / number_of_trials)
-#
-#
-# # ------------------------is_sorted trials with random input below----------------------------
-#
-# max_value = 100
-# number_of_trials = 1000  # you can change this number to make the testing faster.
-# number_of_trials_per_input = 1000
-# # Setting the seed to make sure that the same random list is generated every time with the same parameters
-# random.seed(123)  # If you comment this line, you will get different list every time for the same parameters
-# print("Instancing input...")
-# input_list = [([int(random.random() * max_value) for i in range(number_of_trials)]) for i in range(number_of_trials)]
-# input_list_order = [random.choice(["asc", "desc"]) for i in range(number_of_trials)]
-#
-# print("Input instanced.")
-# print("is_sorted trials commence with random list:")
-# # Time taken in is_sorted
-# total_time_taken = 0
-# for item_list, item_order in zip(input_list, input_list_order):  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers.is_sorted(item_list, item_order)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print('Average time taken by is_sorted using for:', (total_time_taken) / number_of_trials)
-#
-# # Time taken in is_sorted_while
-# # we will use the same setup and input list as recursion so the comparison is fair
-# total_time_taken = 0
-# for item_list, item_order in zip(input_list, input_list_order):  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers.is_sorted_while(item_list, item_order)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print('Average time taken by is_sorted using while:', (total_time_taken) / number_of_trials)
 
-# -----------------------is_prime trials-------------------------------
-
-# trial_text = ("is_prime iterative vs recursive", "is_prime iterative", "is_prime recursive")
-#
-# max_value = 100
-# number_of_trials = 10000  # you can change this number to make the testing faster.
-# number_of_trials_per_input = 10000
-# # Setting the seed to make sure that the same random list is generated every time with the same parameters
-# random.seed(124)  # If you comment this line, you will get different list every time for the same parameters
-# print("Instancing input...")
-# input_list = [int(random.random() * max_value) for i in range(number_of_trials)]
-#
-# print("Input instanced.")
-# print(trial_text[0], "trials commence:")
-# # Time taken in is_sorted
-# total_time_taken = 0
-# for item in input_list:  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers.is_prime(item)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print('Average time taken by', trial_text[1] , ":", (total_time_taken) / number_of_trials)
-#
-# # Time taken in is_sorted_while
-# # we will use the same setup and input list as recursion so the comparison is fair
-# total_time_taken = 0
-# for item in input_list:  # run for all the input
-#     total_time_taken_current_trial = 0
-#     for trial in range(number_of_trials_per_input):  # now run multiple times for every input
-#         start_time = time.time()
-#         helpers_recursive.is_prime(item)
-#         end_time = time.time()
-#         time_taken_current_trial = end_time - start_time
-#         total_time_taken_current_trial = total_time_taken_current_trial + time_taken_current_trial
-#     total_time_taken = total_time_taken + total_time_taken_current_trial
-#
-# print('Average time taken by', trial_text[2] + ":", (total_time_taken) / number_of_trials)
-
-
-# ------------------------------------------------------------------------------------------------
 random.seed()
-number_of_trials = 100000
-a = random.randint(0, 360)
+number_of_trials = 10000000
 
+print('Start of a input')
+a = random.randint(0, 359)
 a_input = []
 for trial in range(number_of_trials):
-    a = random.randint(0, 360)
+    a = random.randint(0, 359)
     a_input.append(a)
-
+print('End of a input')
+print('Start of b input')
 a_index = 0
 b_input = []
 for trial in range(number_of_trials):
-    b = random.randint(0, 360)
+    b = random.randint(0, 359)
     while a_input[a_index] == b:
-        b = random.randint(0, 360)
+        b = random.randint(0, 359)
     b_input.append(b)
     a_index += 1
-
+print('End of b input')
+print('Start of c input')
 c_input = []
 for trial in range(number_of_trials):
-    c = random.randint(0, 360)
+    c = random.randint(0, 359)
     c_input.append(c)
-
+print('End of c input')
+print('Start of d input')
 c_index = 0
 d_input = []
 for trial in range(number_of_trials):
-    d = random.randint(0, 360)
+    d = random.randint(0, 359)
     while c_input[c_index] == d:
-        d = random.randint(0, 360)
+        d = random.randint(0, 359)
     d_input.append(d)
     c_index += 1
-
+print('End of d input')
 input_list = list(zip(a_input, b_input, c_input, d_input))
+print('End of zip')
 results = [0,0]
 result_truth = []
+print('Start of Trials:')
 for trial in input_list:
     current_trial_result = helpers.monty_carlo(trial[0], trial[1], trial[2], trial[3])
     result_truth.append(current_trial_result)
@@ -274,7 +151,6 @@ for trial in input_list:
         results[0] += 1
     else:
         results[1] += 1
-print(input_list)
-print(result_truth)
+print('End of Trials')
 print('Percentage of intersect', ((results[0]/number_of_trials) * 100))
 print('Percentage of non-intersect', ((results[1]/number_of_trials) * 100))
