f6147ab95a3f0452a1468a070be02e3444ebe01f
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-05-13 17:55:56 -0400

    - completed new_bubble_sort -renamed some functions and moved them around - added processing to user input for find_bill_combos - possible problem with storing combinations with find_bill_combos, if there is more than one combination that results have all the same values but are technically different bills

--- helpers.py
+++ helpers.py
@@ -5,21 +5,28 @@ import time
 
 def is_prime(whole_number):
     # Returns True if whole_number is prime and False if not prime
-    divisor = 3
+    divisor = 2
 
-    if whole_number < 2 or whole_number % 2 == 0 and whole_number != 2 or whole_number % 5 == 0:
-        return False
-    elif whole_number == 2:
+    if whole_number == 2:
+        return True
+    while whole_number % divisor !=0 and divisor < sqrt(whole_number):
+        divisor += 1
+    if divisor > sqrt(whole_number):
         return True
     else:
-        while whole_number % divisor != 0 and divisor < sqrt(whole_number):
-            divisor += 2
-            if divisor % 5 == 0:
-                divisor += 2
-        if divisor > sqrt(whole_number):
-            return True
-        else:
-            return False
+        return False
+
+
+def is_prime_range(p, q):
+    result = []
+    for number in range(p, q + 1):
+        for divisor in range(2, int(math.sqrt(number))+1):
+            if number % divisor == 0:
+                break
+            elif divisor > math.sqrt(number)-1:
+                result.append(number)
+
+    return result
 
 
 def my_sum(list_of_items):
@@ -36,39 +43,6 @@ def factorial(whole_number):
     for multiple in range(whole_number - 1, 1, -1):
         accumulator *= multiple
     return accumulator
-
-
-def bubble_sort(list_of_items, order):
-    # Function will return a given list in an ordered list either ascending or descending
-    # The flag variable remains False until the list has finished sorting
-    if order == "ascending" or order == "asc":
-        sorted_flag = False
-        while sorted_flag == False:
-            index = 0
-            sorted_flag = True
-            while index < len(list_of_items) - 1:
-                if list_of_items[index] > list_of_items[index + 1]:
-                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
-                    list_of_items[index + 1] = list_of_items[index] ^ list_of_items[index + 1]
-                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
-                    sorted_flag = False
-                index += 1
-
-
-    elif order == "descending" or order == "desc":
-        sorted_flag = False
-        while sorted_flag == False:
-            index = 0
-            sorted_flag = True
-            while index < len(list_of_items) - 1:
-                if list_of_items[index] < list_of_items[index + 1]:
-                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
-                    list_of_items[index + 1] = list_of_items[index] ^ list_of_items[index + 1]
-                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
-                    sorted_flag = False
-                index += 1
-
-    return list_of_items
 
 
 def find_min_max(list_of_items, min_or_max):
@@ -129,7 +103,7 @@ def sort_using_min_max(list_of_items, order):
         raise ValueError("order parameter must be asc or desc")
 
 
-def is_sorted_while(list_of_elements, order):
+def is_sorted(list_of_elements, order):
     # This function is faster than the is_sorted function which uses for loops instead of while
     # when the passed list is not sorted.
     if order == "ascending" or order == "asc":
@@ -151,50 +125,77 @@ def is_sorted_while(list_of_elements, order):
         return True
 
 
-def is_sorted(list_of_elements, order):
-    # This function using for loops is significantly faster than using a while loop if the list is sorted
-    # However it is slower than using a while loop if the list is unsorted because the for loop
-    # creates an identical list with the first element missing to compare values
-    # This also uses more space than the while loop because it holds two lists in memory
+def bubble_sort(list_of_items, order):
+    # Function will return a given list in an ordered list either ascending or descending
+    # The flag variable remains False until the list has finished sorting
+    max_index = len(list_of_items) - 1
+    passes = 0
+    comparisons = 0
+    swap = 0
     if order == "ascending" or order == "asc":
-        comparing_list = list_of_elements[1:]
-        for first, second in zip(list_of_elements, comparing_list):
-            if first > second:
-                return False
-        return True
+        sorted_flag = False
+        while sorted_flag == False:
+            index = 0
+            sorted_flag = True
+            while index < max_index:
+                if list_of_items[index] > list_of_items[index + 1]:
+                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
+                    list_of_items[index + 1] = list_of_items[index] ^ list_of_items[index + 1]
+                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
+                    sorted_flag = False
+                    swap += 1
+                comparisons +=1
+                index += 1
+            passes += 1
+            max_index -= 1
 
-    if order == "descending" or order == "desc":
-        comparing_list = list_of_elements[1:]
-        for first, second in zip(list_of_elements, comparing_list):
-            if first < second:
-                return False
-        return True
+    elif order == "descending" or order == "desc":
+        sorted_flag = False
+        while sorted_flag == False:
+            index = 0
+            sorted_flag = True
+            while index < max_index:
+                if list_of_items[index] < list_of_items[index + 1]:
+                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
+                    list_of_items[index + 1] = list_of_items[index] ^ list_of_items[index + 1]
+                    list_of_items[index] = list_of_items[index] ^ list_of_items[index + 1]
+                    sorted_flag = False
+                index += 1
+            max_index -= 1
+    print(f'passes: {passes}, comparisons: {comparisons}, swaps {swap}')
+    del passes, comparisons, swap
+    return list_of_items
 
 
 def new_bubble_sort(list):
     maximum_right_index = len(list) - 1
     minimum_left_index = 0
-    while not is_sorted_while(list, "asc") and maximum_right_index != minimum_left_index:
-        left_index = minimum_left_index
-        right_index = left_index + 1
-        while right_index <= maximum_right_index:
-            if list[left_index] > list[right_index]:
-                list[left_index] = list[left_index] ^ list[right_index]
-                list[right_index] = list[left_index] ^ list[right_index]
-                list[left_index] = list[left_index] ^ list[right_index]
-            left_index += 1
-            right_index += 1
+    comparisons = 0
+    swap = 0
+    back_and_forth = 0
+    while minimum_left_index < maximum_right_index:
+        index = minimum_left_index
+        while index < maximum_right_index:
+            if list[index] > list[index + 1]:
+                list[index] = list[index] ^ list[index + 1]
+                list[index + 1] = list[index] ^ list[index + 1]
+                list[index] = list[index] ^ list[index + 1]
+                swap += 1
+            comparisons += 1
+            index += 1
+
         maximum_right_index -= 1
-        right_index = maximum_right_index
-        left_index = right_index - 1
-        while left_index >= minimum_left_index:
-            if list[left_index] > list[right_index]:
-                list[right_index] = list[left_index] ^ list[right_index]
-                list[left_index] = list[left_index] ^ list[right_index]
-                list[right_index] = list[left_index] ^ list[right_index]
-            left_index -= 1
-            right_index -= 1
+        while index > minimum_left_index:
+            if list[index] < list[index - 1]:
+                list[index] = list[index] ^ list[index - 1]
+                list[index - 1] = list[index] ^ list[index - 1]
+                list[index] = list[index] ^ list[index - 1]
+                swap += 1
+            index -= 1
+            comparisons += 1
         minimum_left_index += 1
+        back_and_forth +=1
+    print(f'back and forth: {back_and_forth}, comparisons: {comparisons}, swaps {swap}')
     return list
 
 
@@ -212,7 +213,8 @@ def combinations(list):
             combination_count += 1
     pass
 
-def rotate_90_matrix(matrix):
+
+def transpose_matrix(matrix):
     row_index = 0
     while row_index < len(matrix):
         column_index = 0
@@ -223,6 +225,7 @@ def rotate_90_matrix(matrix):
             column_index += 1
         row_index += 1
     return matrix
+
 
 def print_column_row(matrix):
 
@@ -241,46 +244,52 @@ def print_column_row(matrix):
         column += 1
 
 
-def is_prime_range(p, q):
-    result = []
-    for number in range(p, q + 1):
-        for divisor in range(2, int(math.sqrt(number))+1):
-            if number % divisor == 0:
-                break
-            elif divisor > math.sqrt(number)-1:
-                result.append(number)
-
-    return result
-
-
-def merge(list_1, list_2):
+def merge(list_1, list_2, order):
 
     list_1_index = 0
     list_2_index = 0
     # this temp list will be used to return the final sorted list in this function
     temp_list_2 = []
+    if order == 'desc':
+        # the items in each list are being compared one by one and once
+        # one of the indexes reaches its respective list's end then it exits
+        while list_1_index < len(list_1) and list_2_index < len(list_2):
+            if list_1[list_1_index] > list_2[list_2_index]:
+                temp_list_2.append(list_1[list_1_index])
+                list_1_index += 1
+            else:
+                temp_list_2.append(list_2[list_2_index])
+                list_2_index += 1
 
-    # the items in each list are being compared one by one and once
-    # one of the indexes reaches its respective list's end then it exits
-    while list_1_index < len(list_1) and list_2_index < len(list_2):
-        if list_1[list_1_index] > list_2[list_2_index]:
-            temp_list_2.append(list_1[list_1_index])
-            list_1_index += 1
+        # this if statement will append all remaining items in the list that the index is not at the end
+        if list_1_index == len(list_1):
+            for element in list_2[list_2_index:]:
+                temp_list_2.append(element)
         else:
-            temp_list_2.append(list_2[list_2_index])
-            list_2_index += 1
-
-    # this if statement will append all remaining items in the list that the index is not at the end
-    if list_1_index == len(list_1):
-        for element in list_2[list_2_index:]:
-            temp_list_2.append(element)
+            for element in list_1[list_1_index:]:
+                temp_list_2.append(element)
     else:
-        for element in list_1[list_1_index:]:
-            temp_list_2.append(element)
+        # the items in each list are being compared one by one and once
+        # one of the indexes reaches its respective list's end then it exits
+        while list_1_index < len(list_1) and list_2_index < len(list_2):
+            if list_1[list_1_index] < list_2[list_2_index]:
+                temp_list_2.append(list_1[list_1_index])
+                list_1_index += 1
+            else:
+                temp_list_2.append(list_2[list_2_index])
+                list_2_index += 1
+
+        # this if statement will append all remaining items in the list that the index is not at the end
+        if list_1_index == len(list_1):
+            for element in list_2[list_2_index:]:
+                temp_list_2.append(element)
+        else:
+            for element in list_1[list_1_index:]:
+                temp_list_2.append(element)
     return temp_list_2
 
 
-def merge_sort(input_list):
+def merge_sort(input_list, order:str):
     temp_list = input_list[:]
     # these two assignment statements split the list in half
     temp_list_left = temp_list[:(len(temp_list) // 2)]
@@ -289,30 +298,66 @@ def merge_sort(input_list):
     # If the left list has one element and the right list has more than one element
     # then the right list needs to be split further
     if len(temp_list_left) == 1 and len(temp_list_right) != 1:
-        temp_list_right = merge_sort(temp_list_right)
+        temp_list_right = merge_sort(temp_list_right, order)
 
     # If the right list has one element and the left list has more than one element
     # then the left list needs to be split further
     elif len(temp_list_left) != 1 and len(temp_list_right) == 1:
-        temp_list_left = merge_sort(temp_list_left)
+        temp_list_left = merge_sort(temp_list_left, order)
 
     # If both left and right lists have more than one element then both must be split
     elif len(temp_list_left) != 1 and len(temp_list_right) != 1:
-        temp_list_left = merge_sort(temp_list_left)
-        temp_list_right = merge_sort(temp_list_right)
+        temp_list_left = merge_sort(temp_list_left, order)
+        temp_list_right = merge_sort(temp_list_right, order)
 
     # Once all elements are split up into their smallest parts then this statement will run and
     # go back up the stack to return the result
-    return merge(temp_list_left, temp_list_right)
+    return merge(temp_list_left, temp_list_right, order)
+
+
+def processing(text: str, wanted_characters: str):
+    '''
+    Removes all unwanted characters and excess spaces
+    '''
+    has_spaces = False
+    # replaces unwanted characters and with a space
+    for character in text:
+        if character not in wanted_characters:
+            text = text.replace(character, ' ')
+        elif character == ' ':
+            has_spaces = True
+    # This loop removes all consecutive spaces
+    if has_spaces:
+        while True:
+            max_spaces = 0
+            current_max_spaces = 0
+            for character_index in range(len(text) - 1):
+                if text[character_index] != ' ' and text[character_index + 1] == ' ':
+                    current_max_spaces = 1
+                elif text[character_index] == ' ' and text[character_index + 1] == ' ':
+                    current_max_spaces += 1
+                else:
+                    if current_max_spaces > max_spaces:
+                        max_spaces = current_max_spaces
+                    else:
+                        current_max_spaces = 0
+            if max_spaces % 2 == 0:
+                text = text.replace('  ', ' ')
+            elif max_spaces % 2 != 0 and max_spaces != 1:
+                text = text.replace('   ', ' ')
+            else:
+                break
+    return text
 
 
 def find_bill_combos():
+    """
+    This function is for finding combinations of bills that add to a specific sum
+    """
     wanted_characters = '0123456789. '
-
     bill_prices = input('Enter all bills to compare:\n')
-    for character in bill_prices:
-        if character not in wanted_characters:
-            bill_prices = bill_prices.replace(character, ' ')
+
+    bill_prices = processing(bill_prices, wanted_characters)
     bill_list = bill_prices.split(' ')
 
     # This loop changes all the input into float data type for later comparison
@@ -323,12 +368,11 @@ def find_bill_combos():
 
     # we are sorting the list in descending order because we can infer information at certain points
     # that allow the function to have less iterations
-    bill_list = merge_sort(bill_list)
+    # bill_list_unordered = bill_list[:]
+    bill_list = merge_sort(bill_list, order='desc')
 
-    bill_sum = input('What is the sum you are looking for?')
-    for character in bill_sum:
-        if character not in wanted_characters:
-            bill_sum = bill_sum.replace(character, '')
+    bill_sum = input('What is the sum you are looking for?\n')
+    bill_sum = processing(bill_sum, wanted_characters)
     bill_sum = float(bill_sum)
 
     possible_combinations = []
@@ -338,14 +382,14 @@ def find_bill_combos():
     while starting_index < len(bill_list):
         compare_index = starting_index + 1
 
-        # here we are checking if the bills entered will sum to the wanted sum to begin with
+        # here we are getting the maximum possible sum from the starting bill to the lowest bill
         max_sum = bill_list[starting_index]
         while compare_index < len(bill_list):
             max_sum = max_sum + bill_list[compare_index]
             compare_index += 1
         compare_index = starting_index + 1
 
-        # if it is possible for the bills to sum then we check further
+        # if it is possible for the bills to sum to bill_sum, then we check further
         if max_sum > bill_sum > bill_list[starting_index]:
             # Assume there is a possible combination that starts with the starting index
             possible_combinations.append([bill_list[starting_index]])
@@ -362,9 +406,7 @@ def find_bill_combos():
                     possible_combinations[-1].append(bill_list[compare_index])
                     current_sum += bill_list[compare_index]
 
-                # if the current_sum is greater than the bill_sum there may still be a combination then we can infer that that item cannot be
-                # part of the combination and any further combinations with that number will be greater than
-                # the wanted sum. therefore remove that possibility and start again at the next "inner" index
+                # if the current_sum is greater than the bill_sum there may still be a combination
                 elif current_sum > bill_sum:
 
                     # if we reached the last element in the bill list then there are no more elements to compare with
@@ -446,14 +488,14 @@ if __name__ == "__main__":
     #
     # # Test Cases for is_prime-------------------------------------------------
     # if not is_prime(9):
-    #     print("Passed is_prime(2)")
+    #     print("Passed is_prime(9)")
     # else:
-    #     print("Failed is_prime(2)")
+    #     print("Failed is_prime(9)")
     #
     # if is_prime(113):
-    #     print("Passed is_prime(25)")
+    #     print("Passed is_prime(113)")
     # else:
-    #     print("Failed is_prime(25)")
+    #     print("Failed is_prime(113)")
     #
     # # Test Cases for factorial-------------------------------------------------
     # if factorial(3) == 6:
@@ -486,13 +528,13 @@ if __name__ == "__main__":
     #     print("Passed sort")
     # else:
     #     print("Failed sort", bubble_sort([11, 5, 12, 6], "asc"))
-    #
+    # #
     # if bubble_sort([5, 4, 3, 2, 1], "asc") == [1, 2, 3, 4, 5]:
     #     print("Passed sort")
     # else:
     #     print("Failed sort", bubble_sort([5, 4, 3, 2, 1], "asc"))
-    #
-    # print(new_bubble_sort([2,1,23,4,7,6,5,40,15,31,26,29]))
+    # print(bubble_sort([17,16,15,14,13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 'asc'))
+    # print(new_bubble_sort([17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]))
     # print("End")
     # p = 1
     # q = 30
